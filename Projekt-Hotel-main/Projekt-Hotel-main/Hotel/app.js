555b30716925a15e25e134a652d231abf4a2803e
giganchad <rejzordilda@gmail.com>
2024-10-21 10:28:35 +0200

    Add files via upload

---
+++ Projekt-Hotel-main/Projekt-Hotel-main/Hotel/app.js
@@ -0,0 +1,15 @@
+function printInput(){
+   const imputName = document.querySelector(".name").value
+   const SurName = document.querySelector(".surname").value
+
+   document.querySelector(".resultName").textContent = imputName
+   document.querySelector(".resultSurName").innerHTML = SurName
+}
+
+//let imie = 'pies';
+//imie = 'kot'
+
+//const nazwisko = 'kowalski'
+//nazwisko = 'michalek'
+//console.log(nazwisko)
+//console.log(imie)
